de2717dd10fe7b12be683d8ae6ba0f84f5b6e3c5
malkiro <malkiroshika3@gmail.com>
2023-03-09 05:09:35 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function hiddenContent(){
+    document.getElementById('home').style.display = "block";
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('about').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+}
+
+function load_home(){
+    document.getElementById('home').style.display = "block";
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('about').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+}
+
+function load_about(){
+    document.getElementById('about').style.display = "block";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+}
+
+
+function load_sales(){
+    document.getElementById('sales').style.display = "block";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('about').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+}
+
+
+function load_contact(){
+    document.getElementById('contact').style.display = "block";
+    document.getElementById('home').style.display = "none";
+    document.getElementById('about').style.display = "none";
+    document.getElementById('sales').style.display = "none";
+}
